196194c5e682da3d7ddebc5c1be5c0d3040c135b
pushkin-82 <sputylin@gmail.com>
2019-10-12 16:18:47 +0300

    [HW_02] Add InMemoryUserRepository impl, userId to Meal

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
-    private Integer id;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+
+public class Meal extends AbstractBaseEntity {
+    private final Integer userId;
 
     private final LocalDateTime dateTime;
 
@@ -13,23 +15,17 @@ public class Meal {
 
     private final int calories;
 
+
     public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+        this(null, authUserId(), dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+    public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.userId = userId;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
