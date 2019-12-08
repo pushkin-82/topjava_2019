2b307818dcd14efe47f2d4c391f338df35b6f1dc
pushkin-82 <sputylin@gmail.com>
2019-12-08 22:11:11 +0200

    [HW_09] Add dateTimePicker for meals filter

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -64,4 +64,24 @@ $(function () {
             $.get(mealAjaxUrl, updateTableByData);
         }
     });
+
+    $("#startDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d'
+    });
+    $("#endDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d'
+    });
+    $("#startTime").datetimepicker({
+        datepicker: false,
+        format: 'H:i'
+    });
+    $("#endTime").datetimepicker({
+        datepicker: false,
+        format: 'H:i'
+    });
+    $("#dateTime").datetimepicker({
+        format: 'Y-m-d H:i'
+    });
 });
